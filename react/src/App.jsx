33af60f70ef5f548e0c86ea3e30f773746822ac9
Nishinoyama <begoingto1414@gmail.com>
2024-09-27 11:49:47 +0900

    add swapping product

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css'
 import Matchinglist from './Matchinglist.jsx';
+import TodoList from "./TodoList.jsx";
+import {useState} from "react";
 
 function App() {
+    const [mode, setMode] = useState("unit")
+
+    function changeMode() {
+        setMode(mode === "unit" ? "bulk" : "unit")
+    }
+
     return (
         <>
             <header className="header">
                 <h1>ゼロメモ！！！！！！！</h1>
             </header>
-            <Matchinglist />
+            <button onClick={changeMode}>
+                製品切替 「{mode === "unit" ? "単品マッチ" : "リストマッチ"}」
+            </button>
+            <div hidden={mode !== "bulk"}>
+                <Matchinglist/>
+            </div>
+            <div hidden={mode !== "unit"}>
+                <TodoList/>
+            </div>
         </>
     )
 }
