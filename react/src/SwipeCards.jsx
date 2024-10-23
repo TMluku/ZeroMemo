5c61858a55cd5d2f403828aa97a5e1352e565cae
Nishinoyama <begoingto1414@gmail.com>
2024-10-23 13:58:32 +0900

    add swipe button

--- react/src/SwipeCards.jsx
+++ react/src/SwipeCards.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import TinderCard from "react-tinder-card";
 import * as PropTypes from "prop-types";
 import './SwipeCards.css';
@@ -26,7 +26,6 @@ const item_names = [
     "鶏肉",
 ]
 
-
 export default function SwipeCards({onAddItem, itemList}) {
     const items = item_names
         .filter(name => !itemList.some(item => item.name === name))
@@ -35,27 +34,58 @@ export default function SwipeCards({onAddItem, itemList}) {
             url: `./${name}.png`,
         }));
     const [cards] = useState(items);
-    const [left, setLeft] = useState(cards.length)
+    const [left, setLeft] = useState(cards.length);
 
-    const outOfFrame = (dir, name) => {
+    const [currentIndex, setCurrentIndex] = useState(cards.length - 1);
+    const currentIndexRef = React.useRef(currentIndex);
+
+    const childRefs = React.useMemo(
+        () =>
+            Array(cards.length)
+                .fill(0)
+                .map(() => React.createRef()),
+        [cards.length]
+    );
+
+    const updateCurrentIndex = (index) => {
+        currentIndexRef.current = index;
+        setCurrentIndex(index);
+    }
+
+    const canSwipe = currentIndex >= 0;
+
+    const swiped = (dir, nameToDelete, index) => {
+        updateCurrentIndex(index - 1);
+    }
+
+    const outOfFrame = (dir, name, index) => {
         if (dir === 'right') {
             onAddItem({id: 0, category: '食料品', name: name, selected: false});
         }
         setLeft(left - 1)
+        currentIndexRef.current >= index && childRefs[index].current.restoreCard();
+    }
+
+    const swipe = async (dir) => {
+        if (canSwipe && currentIndex < cards.length) {
+            await childRefs[currentIndex].current.swipe(dir);
+        }
     }
 
     return (
         <div>
             <h2>
-                のこり{left}個
+                のこり{currentIndex + 1}枚
             </h2>
             <div className="cardContainer">
                 {cards.map((card, i) => (
                     <TinderCard
+                        ref={childRefs[i]}
                         className={'swipe'}
                         key={i}
                         onCardLeftScreen={(dir) => outOfFrame(dir, card.name)}
                         preventSwipe={['up', 'down']}
+                        onSwipe={(dir) => swiped(dir, card.name, i)}
                     >
                         <div
                             style={{backgroundImage: `url(${card.url})`}}
@@ -66,6 +96,8 @@ export default function SwipeCards({onAddItem, itemList}) {
                     </TinderCard>
                 ))}
             </div>
+            <button onClick={() => swipe('left')}>左にスワイプ</button>
+            <button onClick={() => swipe('right')}>右にスワイプ</button>
         </div>
     );
 }
